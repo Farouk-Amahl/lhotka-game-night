acefa4465c07e996361f20eee3e024ab586f0194
arthur <arthurmiossec@free.fr>
2022-01-06 13:50:18 +0100

    pour tester les mobils

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -5,8 +5,12 @@ function Options({label, sortByNbrPlayers}) {
   return (
     <div className="optionWrapper">
       <div className="Options">
-        <span>0123456</span>
-        <input type="range" id="nbrPlayers" min="0" max="6" step="1" onChange={(e) => sortByNbrPlayers(e.target.value)}/>
+        <div className="optionInner">
+          <input type="range" id="nbrPlayers" min="0" max="6" step="1" onChange={(e) => sortByNbrPlayers(e.target.value)}/>
+          <div className="range">
+            <span>0</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span>
+          </div>
+        </div>
       </div>
       <div className="optionLabel">
         {label}
