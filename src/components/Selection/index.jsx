77ade633ec8b7c6636d44633341a9b7a6140d945
Arthur <arthurmiossec@free.fr>
2026-01-19 21:19:33 +0100

    tested

--- src/components/Selection/index.jsx
+++ src/components/Selection/index.jsx
@@ -35,7 +35,7 @@ function Selection({ selection, updateSelection, userName }) {
     });
     setGameSelected(tempArray);
     console.log("Selection", tempArray);
-    if (tempArray.length === 1) {
+    if (tempArray.length === 0) {
       setVisibleSelection(false);
     } else {
       setVisibleSelection(true);
